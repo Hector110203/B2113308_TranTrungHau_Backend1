ae266e351d12efcb2022b1761ba6e9e511a313a0
Hector110203 <hautrung110203@gmail.com>
2024-09-19 20:11:25 +0700

    Cai dat su ly loi

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const ApiError = require("./app/api-error.js");
+const contactsRouter = require("./app/routes/contact.route.js");
+
 const app = express();
 
 app.use(cors());
@@ -11,5 +14,17 @@ app.get("/", (req, res) => {
         message: "Welcome to contact book application"
     })
 });
+
+app.use("/api/contacts",contactsRouter)
+
+app.use((req, res, next)=>{
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next)=>{
+    return res.status(err.status || 500).json({
+        message : err.message || "Internal Server Error"
+    })
+})
 
 module.exports = app;
